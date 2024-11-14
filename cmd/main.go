1f564c160d9ad95f0413363f0b156d1ed4d0f280
Gabriel Freitas <gabriielfs96@gmail.com>
2024-11-14 19:23:58 -0300

    feat: added server

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/FreitasGabriel/chat-app/config/logger"
-	"github.com/FreitasGabriel/chat-app/internal/routes"
+	"github.com/FreitasGabriel/chat-app/internal/handler"
+	"github.com/FreitasGabriel/chat-app/internal/service"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	logger.Info("Starting server...")
 
+	websocketBroadcast := handler.NewWebsocketBroadcast()
+
+	gin.SetMode(gin.ReleaseMode)
 	c := gin.Default()
 
-	routes.Init(&c.RouterGroup)
+	c.GET("/health", handler.Health)
+	c.GET("/ws", websocketBroadcast.HandleWebsocketConnection)
+	c.StaticFS("/chat", http.Dir("./templates"))
+
+	go service.WriteMessageOnWebsocket(websocketBroadcast.Broadcast, websocketBroadcast.Clients)
 
 	if err := c.Run(":8000"); err != nil {
 		logger.Error("Error to start server", err)
